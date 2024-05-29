2ece5e724ff2eb4ba70bf7b6f62b2cb59933171d
Marie, Vincent (ITDV XE) - AF <vimarie@airfrance.fr>
2024-05-29 15:30:35 +0200

    fsdhqfsk

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,17 @@
 import { RouterView } from 'vue-router'
 import Header from '@/components/global/Header.vue';
 import Footer from '@/components/global/Footer.vue';
+import moment from 'moment';
+
+const getCurrentDate = () => {
+  return moment().format('YYYY-MM-DD');
+}
 </script>
 
 <template>
   <Header />
   <RouterView />
-  <Footer />
+  <Footer :currentDate="getCurrentDate()" />
 </template>
+
+
